d1261b81dd312f8dd34c555d4ee39446e3e3db75
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-09 23:20:54 +0100

    Changed map co-ordinates

--- app/views/pages/contact.blade.php
+++ app/views/pages/contact.blade.php
@@ -113,14 +113,14 @@
 function initialize() {
   var mapOptions = {
     zoom: 15,
-    center: new google.maps.LatLng(49.1678136, 16.5671893),
+    center: new google.maps.LatLng(50.800680, -1.987766),
     mapTypeId: google.maps.MapTypeId.ROAD,
     scrollwheel: false
   }
   var map = new google.maps.Map(document.getElementById('map'),
     mapOptions);
 
-  var myLatLng = new google.maps.LatLng(49.1681989, 16.5650808);
+  var myLatLng = new google.maps.LatLng(50.800680, -1.987766);
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map
