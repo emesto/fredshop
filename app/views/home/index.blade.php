e9fbaab43e0b588078bb4f538b2bb213739ed93f
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-09 21:38:02 +0100

    Tidying up layout with blade

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -610,3 +610,34 @@
 
 </div><!-- /#navbar -->
 @stop
+@section('javascript-files')
+{{ HTML::script('js/respond.min.js') }}
+{{ HTML::script('js/jquery-1.11.0.min.js') }}
+{{ HTML::script('js/bootstrap.min.js') }} 
+{{ HTML::script('js/jquery.cookie.js') }}  
+{{ HTML::script('js/front.js') }} 
+@stop
+@section('scripts')
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Merriweather:400,400italic' rel='stylesheet' type='text/css'>
+
+{{ HTML::style('css/font-awesome.css') }}
+{{ HTML::style('css/font-awesome.css') }}
+{{ HTML::style('css/bootstrap.css') }}
+{{ HTML::style('css/style.pink.css', array('id' => 'minimal-stylesheet')) }}
+{{ HTML::style('css/owl.carousel.css') }}
+{{ HTML::style('css/owl.theme.css') }}
+
+{{ HTML::script('js/respond.min.js') }}
+
+<link rel="shortcut icon" href="favicon.png" >
+@stop
+@section('meta')
+<meta charset="utf-8" >
+<meta name="robots" content="all,follow" >
+<meta name="googlebot" content="index,follow,snippet,archive" >
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Minimal |e-commerce template</title>
+
+<meta name="keywords" content="" >
+@stop
