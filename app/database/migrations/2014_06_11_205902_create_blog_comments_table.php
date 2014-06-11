04f8c7d6090827ccd258f95e023c499638d72193
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-11 22:03:20 +0100

    Modded migration for comments

---
+++ app/database/migrations/2014_06_11_205902_create_blog_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogCommentsTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('blog_comments', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+      $table->unsignedInteger('user_id');
+      $table->foreign('user_id')->references('id')->on('users');
+
+      $table->unsignedInteger('post_id');
+      $table->foreign('post_id')->references('id')->on('blog_posts');
+
+      $table->longText('content');
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('blog_comments');
+	}
+
+}
