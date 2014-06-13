eb6ffc182c08a1bd3066ba45f0e955079f2b0c12
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-13 23:17:55 +0100

    Refactor

--- app/models/ShopCategory.php
+++ app/models/ShopCategory.php
@@ -5,14 +5,13 @@ class ShopCategory extends Eloquent {
   protected $table = 'shop_categories';
   
   public static function build_category_list($parent_id, &$category_list = array()){
-    $shop_categories = ShopCategory::all();
+    $shop_categories = ShopCategory::where('parent_id', '=', $parent_id)->get();
     foreach($shop_categories as $shop_category){
-      if($shop_category->parent_id == $parent_id){
-        array_push($category_list, $shop_category->name);
-        $temp_list = ShopCategory::build_category_list($shop_category->id);
-        if($temp_list != array()){ array_push($category_list, $temp_list); }    
-      }
+      array_push($category_list, $shop_category->name);
+      $temp_list = ShopCategory::build_category_list($shop_category->id);
+      if($temp_list != array()){ array_push($category_list, $temp_list); }    
     }
     return $category_list;
   }
+
 }
