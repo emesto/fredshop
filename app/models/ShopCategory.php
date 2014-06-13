812d3bf3acfe42f526b57b6b2ac64bd678c1b3e4
Freddie Coleman <f.coleman@hotmail.co.uk>
2014-06-13 23:30:47 +0100

    Built depth into the category builder for creation of sub-navigation

--- app/models/ShopCategory.php
+++ app/models/ShopCategory.php
@@ -4,12 +4,23 @@ class ShopCategory extends Eloquent {
 
   protected $table = 'shop_categories';
   
-  public static function build_category_list($parent_id, &$category_list = array()){
+  public static function build_category_list($parent_id, $depth = 0, &$category_list = array()){
     $shop_categories = ShopCategory::where('parent_id', '=', $parent_id)->get();
     foreach($shop_categories as $shop_category){
-      array_push($category_list, $shop_category->name);
-      $temp_list = ShopCategory::build_category_list($shop_category->id);
-      if($temp_list != array()){ array_push($category_list, $temp_list); }    
+      $category_data = array(
+        'id' => $shop_category->id,
+        'name' => $shop_category->name,
+        'depth' => $depth
+        );
+
+      array_push($category_list, $category_data);
+      $temp_list = ShopCategory::build_category_list($shop_category->id, $depth+1);
+      if($temp_list != array()){ 
+        foreach($temp_list as $add){
+          array_push($category_list, $add);
+        }
+        
+      }    
     }
     return $category_list;
   }
